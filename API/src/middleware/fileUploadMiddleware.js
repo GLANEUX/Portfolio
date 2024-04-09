15f6a1344d323c94bdcb88e7a363f67a029220b2
Océane Glaneux <o.glaneux@gmail.com>
2024-04-10 00:12:24 +0200

    modify upload middleware to modify namefile

---
+++ API/src/middleware/fileUploadMiddleware.js
@@ -0,0 +1,27 @@
+const multer = require('multer');
+
+// Définition du stockage pour multer
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './src/uploads/');
+  },
+  filename: function (req, file, cb) {
+    // Générer une suite de nombres aléatoires
+    const randomNumbers = Math.random().toString(36).substring(2, 8);
+    // Récupérer l'extension du fichier
+    const fileExtension = file.originalname.replace(/\s+/g, '_');
+    // Vérifier si l'extension est .jpg ou .png
+
+    // Concaténer la suite de nombres avec le nom d'origine du fichier
+    const modifiedFileName = `${randomNumbers}-${fileExtension}`;
+    cb(null, modifiedFileName);
+  }
+});
+
+// Initialisation de multer avec les options de stockage
+const upload = multer({ storage: storage });
+
+// Middleware pour gérer le téléchargement de fichiers
+const fileUploadMiddleware = upload.single('file');
+
+module.exports = fileUploadMiddleware;
--- API/src/routes/uploadRoutes.js
+++ API/src/routes/uploadRoutes.js
@@ -1,25 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const fileUploadMiddleware = require('../middleware/fileUploadMiddleware');
 const uploadController = require('../controllers/uploadController');
-const multer = require('multer');
 
-
-// Définir le stockage pour multer
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './src/uploads/');
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  }
-});
-
-// Initialiser multer avec les options de stockage
-const upload = multer({ storage: storage });
-
-// Définir la route pour gérer l'upload de fichiers
-router.post('/upload', upload.single('file'), uploadController.uploadFile);
+// Route pour gérer l'upload de fichiers
+router.post('/upload', fileUploadMiddleware, uploadController.uploadFile);
 
 module.exports = router;
-
-
